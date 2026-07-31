cf18dbdf9d59c405584bf869512885d62641b967
Alexander Mills <alexander.d.mills@gmail.com>
2026-07-31 07:57:47 -0500

    claude autosave 2026-07-31T12:57:47Z

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -16,12 +16,9 @@
 #include <geolocator_windows/geolocator_windows.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
 #include <record_windows/record_windows_plugin_c_api.h>
-<<<<<<< HEAD
 #include <screen_retriever_windows/screen_retriever_windows_plugin_c_api.h>
-#include <tray_manager/tray_manager_plugin.h>
-=======
 #include <share_plus/share_plus_windows_plugin_c_api.h>
->>>>>>> origin/main
+#include <tray_manager/tray_manager_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 #include <window_manager/window_manager_plugin.h>
 #include <window_to_front/window_to_front_plugin.h>
